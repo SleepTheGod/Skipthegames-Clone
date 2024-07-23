802acde86d96f7d95924696a4e5738f145f839de
Taylor Christian Newsome <86472964+SleepTheGod@users.noreply.github.com>
2024-07-23 07:10:53 -0400

    Update fetch_ads.php

--- fetch_ads.php
+++ fetch_ads.php
@@ -1,23 +1,59 @@
 <?php
-include 'db.php';
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "personal_ads";
 
-$city = $_GET['city'] ?? '';
-$county = $_GET['county'] ?? '';
-$state = $_GET['state'] ?? '';
-$gender = $_GET['gender'] ?? '';
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Prepare SQL query
-$sql = "SELECT * FROM posts WHERE city LIKE '%$city%' AND county LIKE '%$county%' AND state LIKE '%$state%' AND gender LIKE '%$gender%'";
-$result = $conn->query($sql);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-$ads = array();
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $ads[] = $row;
-    }
+// Build SQL query with filters
+$sql = "SELECT * FROM ads WHERE 1=1";
+$params = [];
+$types = '';
+
+if (!empty($_GET['gender'])) {
+    $sql .= " AND gender = ?";
+    $params[] = $_GET['gender'];
+    $types .= 's';
+}
+
+if (!empty($_GET['city'])) {
+    $sql .= " AND city = ?";
+    $params[] = $_GET['city'];
+    $types .= 's';
+}
+
+if (!empty($_GET['county'])) {
+    $sql .= " AND county = ?";
+    $params[] = $_GET['county'];
+    $types .= 's';
+}
+
+if (!empty($_GET['state'])) {
+    $sql .= " AND state = ?";
+    $params[] = $_GET['state'];
+    $types .= 's';
+}
+
+$stmt = $conn->prepare($sql);
+if ($types) {
+    $stmt->bind_param($types, ...$params);
+}
+$stmt->execute();
+$result = $stmt->get_result();
+
+$ads = [];
+while ($row = $result->fetch_assoc()) {
+    $ads[] = $row;
 }
 
 echo json_encode($ads);
 
+$stmt->close();
 $conn->close();
 ?>
